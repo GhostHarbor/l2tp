0e87a75f36191def02de84274ac75c9fb3500655
GhostHarbor <157000369+GhostHarbor@users.noreply.github.com>
2024-08-12 10:51:14 +0700

    Update l2tp1.0.sh

--- l2tp1.0.sh
+++ l2tp1.0.sh
@@ -5,6 +5,14 @@
 #
 # DO NOT RUN THIS SCRIPT ON YOUR PC OR MAC!
 #
+# The latest version of this script is available at:
+# https://github.com/hwdsl2/setup-ipsec-vpn
+#
+# 
+#
+# This work is licensed under the Creative Commons Attribution-ShareAlike 3.0
+# Unported License: http://creativecommons.org/licenses/by-sa/3.0/
+#
 # Attribution required: please include my name in any derivative and let me
 # know how you have improved it!
 
