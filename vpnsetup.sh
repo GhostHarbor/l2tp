b8097aad68aa6c9e13289210c24f7abde1ccfd8e
GhostHarbor <157000369+GhostHarbor@users.noreply.github.com>
2024-08-21 10:27:42 +0700

    Update vpnsetup.sh

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -5,14 +5,6 @@
 #
 # DO NOT RUN THIS SCRIPT ON YOUR PC OR MAC!
 #
-# The latest version of this script is available at:
-# https://github.com/hwdsl2/setup-ipsec-vpn
-#
-# Copyright (C) 2021-2024 Lin Song <linsongui@gmail.com>
-#
-# This work is licensed under the Creative Commons Attribution-ShareAlike 3.0
-# Unported License: http://creativecommons.org/licenses/by-sa/3.0/
-#
 # Attribution required: please include my name in any derivative and let me
 # know how you have improved it!
 
